47974425d3143fd4b8b5d1581fe52e967a88ad7a
Evgeny Grakovich <grakovich-e@yandex.by>
2023-03-08 17:15:09 +0300

    add rows toadmin panel

--- robot/admin.py
+++ robot/admin.py
@@ -9,8 +9,11 @@ class TelegramUserAdmin(admin.ModelAdmin):
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['title', 'code', 'has_oscar', 'year']
 
+class TgUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'firstname']
+
 
 # admin.site.register(TelegramUser, TelegramUserAdmin)
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Serial, MovieAdmin)
-admin.site.register(TgUser)
+admin.site.register(TgUser, TgUserAdmin)
