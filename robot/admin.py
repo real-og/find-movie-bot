f94ec1ce836d05011594f3abfb4844c0b0f14f6b
Evgeny Grakovich <grakovich-e@yandex.by>
2023-04-09 10:55:02 +0300

    added search box to admin panel

--- robot/admin.py
+++ robot/admin.py
@@ -6,11 +6,14 @@ from .models import TelegramUser, Movie, Serial, TgUser
 class TelegramUserAdmin(admin.ModelAdmin):
     list_display = ['chat_id', 'user', 'id']
 
+
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['title', 'code', 'has_oscar', 'year']
+    search_fields = ['title', 'code']
 
 class TgUserAdmin(admin.ModelAdmin):
     list_display = ['id', 'username', 'firstname']
+    search_fields = ['id', 'username']
 
 
 # admin.site.register(TelegramUser, TelegramUserAdmin)
